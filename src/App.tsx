3d339dc7d7297fe8120d6f9506ab5e82d7502c1d
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-10 16:18:14 +0100

    Add some dummy styling for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { format } from "date-fns";
+import Quote from "./components/Quote";
+import clsx from "clsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const currentDate = new Date();
+  const currentHour = Number(format(currentDate, "H"));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main
+      className={clsx("bg-black/50 bg-blend-darken", {
+        "bg-daytime-mobile md:bg-daytime-tablet xl:bg-daytime-desktop":
+          currentHour < 19,
+        "bg-nighttime-mobile md:bg-nighttime-tablet xl:bg-nighttime-desktop":
+          currentHour >= 19,
+      })}
+    >
+      <Quote />
+    </main>
+  );
 }
 
-export default App
+export default App;
