df98e2dcfa378f582d4705915e283587a0b19fac
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-10 15:56:28 +0100

    Add backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ export default {
         "c-200": "#303030",
         "c-300": "#FFFFFF",
       },
+      backgroundImage: {
+        "daytime-desktop": "url('/desktop/bg-image-daytime.jpg')",
+        "nighttime-desktop": "url('/desktop/bg-image-nighttime.jpg')",
+
+        "daytime-tablet": "url('/tablet/bg-image-daytime.jpg')",
+        "nighttime-tablet": "url('/tablet/bg-image-nighttime.jpg')",
+
+        "daytime-mobile": "url('/mobile/bg-image-daytime.jpg')",
+        "nighttime-mobile": "url('/mobile/bg-image-nighttime.jpg')",
+      },
     },
   },
   plugins: [],
